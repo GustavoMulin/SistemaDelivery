d26a8ac0824e86e26399a0cdf888ae794ff87901
GustavoMulin <gustavobarbosa7824@gmail.com>
2025-11-25 10:56:14 -0400

    [add]: Rotas

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\EnderecoController;
+use App\Http\Controllers\PedidoController;
+use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\RestauranteController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('restaurantes', RestauranteController::class);
+    Route::apiResource('produtos', ProdutoController::class);
+    Route::apiResource('pedidos', PedidoController::class);
+    Route::apiResource('itemspedidos', PedidoController::class);
+    Route::apiResource('enderecos', EnderecoController::class);
+});
