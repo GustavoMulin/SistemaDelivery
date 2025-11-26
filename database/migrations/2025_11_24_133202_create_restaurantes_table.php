b1474ead69012aa305c705010c4d283f3768dcaa
GustavoMulin <gustavobarbosa7824@gmail.com>
2025-11-26 10:20:23 -0400

    [add]: Mudança na migration de restaurante

--- database/migrations/2025_11_24_133202_create_restaurantes_table.php
+++ database/migrations/2025_11_24_133202_create_restaurantes_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('nome');
             $table->text('descricao')->nullable();
             $table->string('endereco');
-            $table->string('telefone')->nullable();
             $table->timestamps();
         });
     }
